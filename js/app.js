bf00aa191eb548307d893ca4ac92a76a5147b878
vershimat <vershima@procurementmonitor.org>
2018-09-11 14:19:15 +0100

    first draft

---
+++ js/app.js
@@ -0,0 +1,234 @@
+UIkit.util.ready(function () {
+        
+    var bar = document.getElementById('abia-info');
+    var y = 0;
+    var x = bar.value;
+    bar.value = 0;
+    var animate = setInterval(function () {
+
+bar.value += 10;
+
+if (bar.value >= x) {
+clearInterval(animate);
+}
+
+}, 1000);
+
+});
+
+UIkit.util.ready(function () {
+                
+    var bar = document.getElementById('anam-ogp');
+    var y = 0;
+    var x = bar.value;
+    bar.value = 0;
+    var animate = setInterval(function () {
+
+        // y += 10;
+        // console.log(y);
+        // bar.value = 0;
+        // if (y >= x) {
+        //     clearInterval(animate);
+        // }
+
+    }, 1000);
+    var animate = setInterval(function () {
+
+bar.value += 1;
+
+if (bar.value >= x) {
+clearInterval(animate);
+}
+
+}, 200);
+
+});
+UIkit.util.ready(function () {
+                
+    var bar = document.getElementById('ebon-ogp');
+    var y = 0;
+    var x = bar.value;
+    bar.value = 0;
+    var animate = setInterval(function () {
+
+        // y += 10;
+        // console.log(y);
+        // bar.value = 0;
+        // if (y >= x) {
+        //     clearInterval(animate);
+        // }
+
+    }, 1000);
+    var animate = setInterval(function () {
+
+bar.value += 1;
+
+if (bar.value >= x) {
+clearInterval(animate);
+}
+
+}, 200);
+
+});
+UIkit.util.ready(function () {
+                
+    var bar = document.getElementById('ebon-info');
+    var y = 0;
+    var x = bar.value;
+    bar.value = 0;
+    var animate = setInterval(function () {
+
+        // y += 10;
+        // console.log(y);
+        // bar.value = 0;
+        // if (y >= x) {
+        //     clearInterval(animate);
+        // }
+
+    }, 1000);
+    var animate = setInterval(function () {
+
+bar.value += 1;
+
+if (bar.value >= x) {
+clearInterval(animate);
+}
+
+}, 200);
+
+});
+UIkit.util.ready(function () {
+                
+    var bar = document.getElementById('enugu-ogp');
+    var y = 0;
+    var x = bar.value;
+    bar.value = 0;
+    var animate = setInterval(function () {
+
+        // y += 10;
+        // console.log(y);
+        // bar.value = 0;
+        // if (y >= x) {
+        //     clearInterval(animate);
+        // }
+
+    }, 1000);
+    var animate = setInterval(function () {
+
+bar.value += 1;
+
+if (bar.value >= x) {
+clearInterval(animate);
+}
+
+}, 200);
+
+});
+UIkit.util.ready(function () {
+                
+    var bar = document.getElementById('ekiti-ogp');
+    var y = 0;
+    var x = bar.value;
+    bar.value = 0;
+    var animate = setInterval(function () {
+
+        // y += 10;
+        // console.log(y);
+        // bar.value = 0;
+        // if (y >= x) {
+        //     clearInterval(animate);
+        // }
+
+    }, 1000);
+    var animate = setInterval(function () {
+
+bar.value += 1;
+
+if (bar.value >= x) {
+clearInterval(animate);
+}
+
+}, 200);
+
+});
+UIkit.util.ready(function () {
+                
+    var bar = document.getElementById('kad-ogp');
+    var y = 0;
+    var x = bar.value;
+    bar.value = 0;
+    var animate = setInterval(function () {
+
+        // y += 10;
+        // console.log(y);
+        // bar.value = 0;
+        // if (y >= x) {
+        //     clearInterval(animate);
+        // }
+
+    }, 1000);
+    var animate = setInterval(function () {
+
+bar.value += 1;
+
+if (bar.value >= x) {
+clearInterval(animate);
+}
+
+}, 200);
+
+});
+UIkit.util.ready(function () {
+                
+    var bar = document.getElementById('kad-info');
+    var y = 0;
+    var x = bar.value;
+    bar.value = 0;
+    var animate = setInterval(function () {
+
+        // y += 10;
+        // console.log(y);
+        // bar.value = 0;
+        // if (y >= x) {
+        //     clearInterval(animate);
+        // }
+
+    }, 1000);
+    var animate = setInterval(function () {
+
+bar.value += 1;
+
+if (bar.value >= x) {
+clearInterval(animate);
+}
+
+}, 200);
+
+});
+UIkit.util.ready(function () {
+                
+    var bar = document.getElementById('kan-ogp');
+    var y = 0;
+    var x = bar.value;
+    bar.value = 0;
+    var animate = setInterval(function () {
+
+        // y += 10;
+        // console.log(y);
+        // bar.value = 0;
+        // if (y >= x) {
+        //     clearInterval(animate);
+        // }
+
+    }, 1000);
+    var animate = setInterval(function () {
+
+bar.value += 1;
+
+if (bar.value >= x) {
+clearInterval(animate);
+}
+
+}, 200);
+
+});
